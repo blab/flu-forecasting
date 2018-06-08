5a1f1a882ad8c2520e8f346627350ec8e0148729
John Huddleston <huddlej@gmail.com>
2018-06-08 15:08:45 -0700

    Use KDE frequencies for fitness model.

--- fit_model.py
+++ fit_model.py
@@ -11,7 +11,7 @@ sys.path.insert(0, code_directory)
 
 # Load augur modules.
 from base.fitness_model import fitness_model as FitnessModel, make_pivots
-from base.frequencies import tree_frequencies as TreeFrequency, logit_transform
+from base.frequencies import KdeFrequencies
 from base.io_util import json_to_tree, json_to_clade_frequencies
 from base.titer_model import TiterCollection
 
@@ -39,6 +39,7 @@ if __name__ == "__main__":
     parser.add_argument("model", help="output model JSON")
     parser.add_argument("predictors", nargs="+", help="one or more predictors to build model for")
     parser.add_argument("--titers", help="tab-delimited file of titer measurements")
+    parser.add_argument("--sigma", type=float, default=1 / 12.0, help="Bandwidth for KDE frequencies")
 
     args = parser.parse_args()
     predictor_kwargs = {}
@@ -53,11 +54,17 @@ if __name__ == "__main__":
     with open(args.frequencies, "r") as json_fh:
         json_frequencies = json.load(json_fh)
 
-    # Convert JSON tree layout to a Biopython Clade instance.
-    frequencies = json_to_clade_frequencies(json_frequencies)
-
     # Get pivots from frequencies JSON.
     pivots = np.array(json_frequencies["pivots"])
+
+    # Use empty frequencies to force the fitness model to recalculate
+    # frequencies with the KDE approach.
+    frequencies = KdeFrequencies.estimate_frequencies_for_tree(
+        tree,
+        pivots,
+        sigmaNarrow=args.sigma,
+        proportionWide=0.0
+    )
 
     # Determine the time interval from the pivots defined in the frequencies JSON.
     start_date = pivot_to_date(min(pivots))
@@ -88,7 +95,8 @@ if __name__ == "__main__":
         epitope_mask_version="wolf",
         tolerance_mask_version="HA1",
         min_freq=0.1,
-        predictor_kwargs=predictor_kwargs
+        predictor_kwargs=predictor_kwargs,
+        sigma=args.sigma
     )
     model.predict()
     model.validate_prediction()
