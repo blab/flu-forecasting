de1f863dae4610f8c98088d70cb6a33b59ab6ea0
John Huddleston <huddlej@gmail.com>
2018-09-24 09:20:39 -0700

    Plot clade LBI by observed growth rate without time facetting and writing out the TSV of the data from the JSON model

--- analyses/identify-optimal-lbi-parameters/plot_lbi_clade_growth.py
+++ analyses/identify-optimal-lbi-parameters/plot_lbi_clade_growth.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument("model", help="JSON for fitness model")
-    parser.add_argument("output", help="figure of clade growth by LBI")
+    parser.add_argument("figure", help="figure of clade growth by LBI")
+    parser.add_argument("table", help="table of clade growth by LBI")
 
     args = parser.parse_args()
 
@@ -24,14 +25,15 @@ if __name__ == "__main__":
 
     df = pd.DataFrame(json_model["data"])
     df["observed_growth"] =  df["observed_freq"] / df["initial_freq"]
+    df.to_csv(args.table, sep="\t", header=True, index=False)
 
-    g = sns.lmplot("lbi", "observed_growth", data=df, col="timepoint", col_wrap=3, height=6, aspect=1.33)
+    g = sns.lmplot("lbi", "observed_growth", data=df, height=6, aspect=1.33, fit_reg=False)
     g.set_axis_labels("LBI", "Observed clade frequency fold change")
 
     for ax in g.axes.flatten():
         ax.axhline(y=1.0, alpha=0.5, color="black")
-        ax.set_xlim(0, 1)
-        ax.set_ylim(0, 8)
+        ax.set_xlim(0, 0.6)
+        ax.set_ylim(-0.5, 8)
 
-    plt.savefig(args.output)
+    plt.savefig(args.figure)
     plt.close()
