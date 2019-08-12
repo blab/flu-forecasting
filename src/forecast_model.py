3fc04a65592a3454dba44941aeb399230dd21f9a
John Huddleston <huddlej@gmail.com>
2019-08-12 15:34:45 -0700

    Add support for forecasting to multiple delta times in the future
    This new interface supports forecasting 3, 6, etc. months into the future and
    outputting the forecast table annotated by the corresponding future
    timepoints. This will allow us to merge forecasted frequencies across multiple
    timepoints with observed frequencies.

--- src/forecast_model.py
+++ src/forecast_model.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     parser.add_argument("--tip-attributes", required=True, help="tab-delimited file describing tip attributes at all timepoints with standardized predictors")
     parser.add_argument("--distances", help="tab-delimited file of distances between pairs of samples")
     parser.add_argument("--model", required=True, help="JSON representing the model fit with training and cross-validation results, beta coefficients for predictors, and summary statistics")
-    parser.add_argument("--delta-months", required=True, type=int, help="number of months to project clade frequencies into the future")
+    parser.add_argument("--delta-months", required=True, type=int, nargs="+", help="number of months to project clade frequencies into the future")
     parser.add_argument("--output", required=True, help="table of forecasts for the given tips")
 
     args = parser.parse_args()
@@ -47,21 +47,30 @@ if __name__ == "__main__":
     coefficients = np.array(model_json["coefficients_mean"])
     mean_stds = np.array(model_json["mean_stds_mean"])
 
-    # For each train/validate split, fit a model to the training data, and
-    # evaluate the model with the validation data, storing the training results,
-    # beta parameters, and validation results.
-    delta_time = args.delta_months / 12.0
-    model = DistanceExponentialGrowthModel(
-        predictors=predictors,
-        delta_time=delta_time,
-        cost_function=cost_function,
-        l1_lambda=l1_lambda,
-        distances=distances_by_sample_names
-    )
-    model.coef_ = coefficients
-    model.mean_stds_ = mean_stds
+    forecasts = []
+    for delta_month in args.delta_months:
+        # For each train/validate split, fit a model to the training data, and
+        # evaluate the model with the validation data, storing the training results,
+        # beta parameters, and validation results.
+        delta_time = delta_month / 12.0
+        delta_offset = pd.DateOffset(months=delta_month)
 
-    # Forecast given tips.
-    forecasts = model.predict(tips)
-    forecasts["weighted_distance_to_future_by_%s" % "-".join(predictors)] = forecasts["y"]
-    forecasts.to_csv(args.output, sep="\t", index=False, header=True)
+        model = DistanceExponentialGrowthModel(
+            predictors=predictors,
+            delta_time=delta_time,
+            cost_function=cost_function,
+            l1_lambda=l1_lambda,
+            distances=distances_by_sample_names
+        )
+        model.coef_ = coefficients
+        model.mean_stds_ = mean_stds
+
+        # Forecast given tips.
+        forecasts_df = model.predict(tips)
+        forecasts_df["weighted_distance_to_future_by_%s" % "-".join(predictors)] = forecasts_df["y"]
+        forecasts_df["future_timepoint"] = forecasts_df["timepoint"] + delta_offset
+
+        forecasts.append(forecasts_df)
+
+    all_forecasts = pd.concat(forecasts, ignore_index=True)
+    all_forecasts.to_csv(args.output, sep="\t", index=False, header=True, na_rep="N/A")
