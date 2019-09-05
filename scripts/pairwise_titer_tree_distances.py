a861d4706369b29c31728c7d9a251d1063dc843f
John Huddleston <huddlej@gmail.com>
2019-09-05 09:56:20 -0700

    Reduce run time for pairwise titer tree distances
    Reduces the maximum number of past samples to consider for comparison to current
    samples and also introduces a minimum frequency that a sample must be or have
    been in the present or past to be included in a calculation. This latter change
    helps us avoid calculations for samples that do not actually contribute to
    forecasts.

--- scripts/pairwise_titer_tree_distances.py
+++ scripts/pairwise_titer_tree_distances.py
@@ -22,7 +22,8 @@ if __name__ == "__main__":
     parser.add_argument("--model", help="JSON providing the titer tree model", required=True)
     parser.add_argument("--date-annotations", help="JSON of branch lengths and date annotations from augur refine for samples in the given tree; required for comparisons to earliest or latest date", required=True)
     parser.add_argument("--years-back-to-compare", type=int, help="number of years prior to the current season to search for samples to calculate pairwise comparisons with", required=True)
-    parser.add_argument("--max-past-samples", type=int, default=500, help="maximum number of past samples to randomly select for comparison to current samples")
+    parser.add_argument("--max-past-samples", type=int, default=200, help="maximum number of past samples to randomly select for comparison to current samples")
+    parser.add_argument("--min-frequency", type=float, default=1e-5, help="minimum frequency to consider a sample alive")
     parser.add_argument("--output", help="JSON file with calculated distances stored by node name and attribute name", required=True)
 
     args = parser.parse_args()
@@ -57,10 +58,10 @@ if __name__ == "__main__":
         # Samples with nonzero frequencies in the last timepoint are current
         # samples. Those with one or more nonzero frequencies in the search
         # window of the past timepoints are past samples.
-        if frequencies.frequencies[tip.name][-1] > 0:
+        if frequencies.frequencies[tip.name][-1] > args.min_frequency:
             current_samples.append(tip.name)
             tips_by_sample[tip.name] = tip
-        elif (frequencies.frequencies[tip.name][past_pivot_indices] > 0).sum() > 0:
+        elif (frequencies.frequencies[tip.name][past_pivot_indices] > args.min_frequency).sum() > 0:
             past_samples.append(tip.name)
             tips_by_sample[tip.name] = tip
 
