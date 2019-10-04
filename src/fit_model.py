01ef543dc1a527d02aa63a9e84af094b75ec7235
John Huddleston <huddlej@gmail.com>
2019-10-04 16:13:49 -0700

    Add logic to calculate test error instead of cross-validation error

--- src/fit_model.py
+++ src/fit_model.py
@@ -734,6 +734,7 @@ def cross_validate(model_class, model_kwargs, data, targets, train_validate_time
             },
             "validation_n": validation_X[group_by].unique().shape[0],
             "validation_error": validation_error,
+            "null_validation_error": null_validation_error,
             "last_training_timepoint": training_timepoints[-1].strftime("%Y-%m-%d"),
             "validation_timepoint": validation_timepoint.strftime("%Y-%m-%d")
         }
@@ -751,25 +752,129 @@ def cross_validate(model_class, model_kwargs, data, targets, train_validate_time
     return results
 
 
-def summarize_cross_validation_scores(scores, include_scores=False):
-    """Summarize model errors including in-sample errors by AIC, out-of-sample
-    errors by cross-validation, and beta parameters across timepoints.
+def test(model_class, model_kwargs, data, targets, timepoints, coefficients=None, group_by="clade_membership",
+                   include_attributes=False):
+    """Calculate test scores for the given data and targets across the given
+    timepoints.
+
+    Parameters
+    ----------
+    model : ExponentialGrowthModel
+        an instance of a model with defined hyperparameters including a list of
+        predictors to use for fitting
+
+    data : pandas.DataFrame
+        standardized input attributes to use for model fitting
+
+    targets : pandas.DataFrame
+        observed outputs to test the model with
+
+    timepoints : list
+        a list of timepoint strings in YYYY-MM-DD format
+
+    coefficients : ndarray
+        an array of fixed coefficients for the given model's predictors
+
+    group_by : string
+        column of the tip attributes by which they should be grouped to
+        calculate the total number of samples in the model (e.g., group by clade
+        or strain)
+
+    include_attributes : boolean
+        specifies whether tip attribute data used to test models should be
+        included in the output per timepoint
+
+    Returns
+    -------
+    list
+        a list of dictionaries containing test results with scores per timepoint
+
+    """
+    results = []
+    differences_of_model_and_naive_errors = []
+
+    for timepoint in timepoints:
+        model = model_class(**model_kwargs)
+        model.coef_ = coefficients
+        model.mean_stds_ = np.zeros_like(coefficients)
+
+        # Get training and validation timepoints.
+        test_timepoint = pd.to_datetime(timepoint)
+
+        # Get test data by timepoints.
+        test_X = data[data["timepoint"] == test_timepoint].copy()
+        test_y = targets[targets["timepoint"] == test_timepoint].copy()
+
+        # Calculate the model score for the validation data.
+        test_error = model.score(test_X, test_y)
+        null_test_error = model._fit(np.zeros_like(model.coef_), test_X, test_y)
+        differences_of_model_and_naive_errors.append(test_error - null_test_error)
+        print(
+            "%s\t%.2f\t%.2f\t%s\t%.2f" % (
+                test_timepoint.strftime("%Y-%m"),
+                test_error,
+                null_test_error,
+                model.coef_,
+                (np.array(differences_of_model_and_naive_errors) < 0).sum() / float(len(differences_of_model_and_naive_errors))
+            ),
+            flush=True
+        )
+
+        # Get the estimated frequencies for test sets to export.
+        test_y_hat = model.predict(test_X)
+
+        # Convert timestamps to a serializable format.
+        for df in [test_X, test_y, test_y_hat]:
+            for column in ["timepoint", "future_timepoint"]:
+                if column in df.columns:
+                    df[column] = df[column].dt.strftime("%Y-%m-%d")
+
+        # Store test results and beta coefficients.
+        result = {
+            "predictors": model.predictors,
+            "coefficients": model.coef_.tolist(),
+            "mean_stds": model.mean_stds_.tolist(),
+            "validation_data": {
+                "y": test_y.to_dict(orient="records"),
+                "y_hat": test_y_hat.to_dict(orient="records")
+            },
+            "validation_n": test_X[group_by].unique().shape[0],
+            "validation_error": test_error,
+            "null_validation_error": null_test_error,
+            "validation_timepoint": test_timepoint.strftime("%Y-%m-%d")
+        }
+
+        # Include tip attributes, if requested.
+        if include_attributes:
+            result["validation_data"]["X"] = test_X.to_dict(orient="records")
+
+        results.append(result)
+
+    # Return results for all validation timepoints.
+    print("Mean difference between model and naive: %.4f" % (sum(differences_of_model_and_naive_errors) / len(differences_of_model_and_naive_errors)), flush=True)
+    print("Proportion of timepoints when model < naive: %.2f" % ((np.array(differences_of_model_and_naive_errors) < 0).sum() / float(len(differences_of_model_and_naive_errors))))
+    return results
+
+
+def summarize_scores(scores, include_scores=False):
+    """Summarize model errors across timepoints.
 
     Parameters
     ----------
     scores : list
         a list of cross-validation results including training errors,
-        cross-validation errors, and beta coefficients
+        cross-validation errors, and beta coefficients OR a list of test errors
 
     include_scores : boolean
         specifies whether cross-validation scores should be included in the
-        output per training window
+        output per timepoint
 
     Returns
     -------
     dict :
         a dictionary of all cross-validation results plus summary statistics for
-        training, cross-validation, and beta coefficients
+        training, cross-validation, and beta coefficients OR test results
+
     """
     summary = {
         "predictors": scores[0]["predictors"]
@@ -819,6 +924,7 @@ def get_errors_by_timepoint(scores):
             "predictors": predictors,
             "validation_timepoint": pd.to_datetime(score["validation_timepoint"]),
             "validation_error": score["validation_error"],
+            "null_validation_error": score["null_validation_error"],
             "validation_n": score["validation_n"]
         })
 
@@ -856,7 +962,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--tip-attributes", required=True, help="tab-delimited file describing tip attributes at all timepoints with standardized predictors")
     parser.add_argument("--output", required=True, help="JSON representing the model fit with training and cross-validation results, beta coefficients for predictors, and summary statistics")
-    parser.add_argument("--predictors", required=True, nargs="+", help="tip attribute columns to use as predictors of final clade frequencies")
+    parser.add_argument("--predictors", nargs="+", help="tip attribute columns to use as predictors of final clade frequencies; optional if a fixed model is provided")
     parser.add_argument("--delta-months", required=True, type=int, help="number of months to project clade frequencies into the future")
     parser.add_argument("--target", required=True, choices=["clades", "distances"], help="target for models to fit")
     parser.add_argument("--final-clade-frequencies", help="tab-delimited file of clades per timepoint and their corresponding tips and tip frequencies at the given delta time in the future")
@@ -869,8 +975,17 @@ if __name__ == "__main__":
     parser.add_argument("--include-scores", action="store_true", help="include score data resulting from cross-validation output")
     parser.add_argument("--errors-by-timepoint", help="optional data frame of cross-validation errors by validation timepoint")
     parser.add_argument("--coefficients-by-timepoint", help="optional data frame of coefficients by validation timepoint")
+    parser.add_argument("--fixed-model", help="optional model JSON to use as a fixed model for calculation of test error in forecasts")
 
     args = parser.parse_args()
+
+    cost_functions_by_name = {
+        "sse": sum_of_squared_errors,
+        "rmse": root_mean_square_error,
+        "mae": mean_absolute_error,
+        "information_gain": negative_information_gain,
+        "diffsum": sum_of_differences
+    }
 
     # Load standardized tip attributes subsetting to tip name, clade, frequency,
     # and requested predictors.
@@ -937,63 +1052,107 @@ if __name__ == "__main__":
     # Identify all available timepoints from tip attributes.
     timepoints = tips["timepoint"].dt.strftime("%Y-%m-%d").unique()
 
-    # Identify train/validate splits from timepoints.
-    train_validate_timepoints = get_train_validate_timepoints(
-        timepoints,
-        args.delta_months,
-        args.training_window
-    )
+    # If a fixed model is provided, calculate test errors. Otherwise, calculate
+    # cross-validation errors.
+    if args.fixed_model is not None:
+        # Load model details and extract mean coefficients.
+        with open(args.fixed_model, "r") as fh:
+            model_json = json.load(fh)
 
-    # Select the cost function.
-    if args.cost_function == "sse":
-        cost_function = sum_of_squared_errors
-    elif args.cost_function == "rmse":
-        cost_function = root_mean_square_error
-    elif args.cost_function == "mae":
-        cost_function = mean_absolute_error
-    elif args.cost_function == "information_gain":
-        cost_function = negative_information_gain
-    elif args.cost_function == "diffsum":
-        cost_function = sum_of_differences
+        coefficients = np.array(model_json["coefficients_mean"])
+        delta_months = model_json["delta_months"]
+        delta_time = delta_months / 12.0
+        l1_lambda = model_json["l1_lambda"]
+        training_window = model_json["training_window"]
+        cost_function_name = model_json["cost_function"]
+        cost_function = cost_functions_by_name[cost_function_name]
+        model_kwargs.update({
+            "predictors": model_json["predictors"],
+            "delta_time": delta_time,
+            "l1_lambda": l1_lambda,
+            "cost_function": cost_function
+        })
 
-    # For each train/validate split, fit a model to the training data, and
-    # evaluate the model with the validation data, storing the training results,
-    # beta parameters, and validation results.
-    delta_time = args.delta_months / 12.0
-    model_kwargs.update({
-        "predictors": args.predictors,
-        "delta_time": delta_time,
-        "l1_lambda": args.l1_lambda,
-        "cost_function": cost_function
-    })
+        # Find the latest timepoint we can project from based on the given delta
+        # months.
+        latest_timepoint = pd.to_datetime(timepoints[-1]) - pd.DateOffset(months=delta_months)
+        test_timepoints = [
+            timepoint
+            for timepoint in timepoints
+            if pd.to_datetime(timepoint) <= latest_timepoint
+        ]
 
-    # If this is a naive model, set the coefficients to zero so cross-validation
-    # can run under naive model conditions.
-    if "naive" in args.predictors:
-        coefficients = np.zeros(len(args.predictors))
+        # Calculate test errors/scores for the given coefficients and data at
+        # the identified test timepoints.
+        scores = test(
+            model_class,
+            model_kwargs,
+            tips,
+            targets,
+            test_timepoints,
+            coefficients,
+            group_by=group_by_attribute,
+            include_attributes=args.include_attributes
+        )
     else:
-        coefficients = None
+        # First, confirm that all predictors are defined in the given tip
+        # attributes.
+        if not all([predictor in tips.columns for predictor in args.predictors]):
+            print("ERROR: Not all predictors could be found in the given tip attributes table.", file=sys.stderr)
+            sys.exit(1)
 
-    scores = cross_validate(
-        model_class,
-        model_kwargs,
-        tips,
-        targets,
-        train_validate_timepoints,
-        coefficients,
-        group_by=group_by_attribute,
-        include_attributes=args.include_attributes
-    )
+        # Select the cost function.
+        cost_function_name = args.cost_function
+        cost_function = cost_functions_by_name[cost_function_name]
+
+        # Identify train/validate splits from timepoints.
+        training_window = args.training_window
+        train_validate_timepoints = get_train_validate_timepoints(
+            timepoints,
+            args.delta_months,
+            training_window
+        )
+
+        # For each train/validate split, fit a model to the training data, and
+        # evaluate the model with the validation data, storing the training results,
+        # beta parameters, and validation results.
+        delta_months = args.delta_months
+        delta_time = delta_months / 12.0
+        l1_lambda = args.l1_lambda
+        model_kwargs.update({
+            "predictors": args.predictors,
+            "delta_time": delta_time,
+            "l1_lambda": l1_lambda,
+            "cost_function": cost_function
+        })
+
+        # If this is a naive model, set the coefficients to zero so cross-validation
+        # can run under naive model conditions.
+        if "naive" in args.predictors:
+            coefficients = np.zeros(len(args.predictors))
+        else:
+            coefficients = None
+
+        scores = cross_validate(
+            model_class,
+            model_kwargs,
+            tips,
+            targets,
+            train_validate_timepoints,
+            coefficients,
+            group_by=group_by_attribute,
+            include_attributes=args.include_attributes
+        )
 
     # Summarize model errors including in-sample errors by AIC, out-of-sample
     # errors by cross-validation, and beta parameters across timepoints.
-    model_results = summarize_cross_validation_scores(scores, args.include_scores)
+    model_results = summarize_scores(scores, args.include_scores)
 
     # Annotate parameters used to produce models.
-    model_results["cost_function"] = args.cost_function
-    model_results["l1_lambda"] = args.l1_lambda
-    model_results["delta_months"] = args.delta_months
-    model_results["training_window"] = args.training_window
+    model_results["cost_function"] = cost_function_name
+    model_results["l1_lambda"] = l1_lambda
+    model_results["delta_months"] = delta_months
+    model_results["training_window"] = training_window
     model_results["pseudocount"] = args.pseudocount
 
     # Save model fitting hyperparameters, raw results, and summary of results to
