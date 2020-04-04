3a0d4bd629e5817478a4720f7e2fdc6efd88bfe1
John Huddleston <huddlej@gmail.com>
2020-04-04 09:37:09 -0700

    Update standardize simulated dates scripts
    This is part of the migration from run to shell blocks.

--- scripts/standardize_simulated_sequence_dates.py
+++ scripts/standardize_simulated_sequence_dates.py
@@ -1,12 +1,34 @@
-"""Concatenate two or more tables as data frames.
+"""Standardize simulated sequence dates to human readable format (YYYY-MM-DD).
 """
 import argparse
+import datetime
 import pandas as pd
+
+
+def float_to_datestring(time):
+    """Convert a floating point date from TreeTime `numeric_date` to a date string
+    """
+    # Extract the year and remainder from the floating point date.
+    year = int(time)
+    remainder = time - year
+
+    # Calculate the day of the year (out of 365 + 0.25 for leap years).
+    tm_yday = int(remainder * 365.25)
+    if tm_yday == 0:
+        tm_yday = 1
+
+    # Construct a date object from the year and day of the year.
+    date = datetime.datetime.strptime("%s-%s" % (year, tm_yday), "%Y-%j")
+
+    # Build the date string with zero-padded months and days.
+    date_string = "%s-%.2i-%.2i" % (date.year, date.month, date.day)
+
+    return date_string
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--metadata", nargs="+", help="")
+    parser.add_argument("--metadata", help="metadata for simulated sequences")
     parser.add_argument("--start-year", default=2000.0, type=float, help="year to start simulated dates from")
     parser.add_argument("--generations-per-year", default=200.0, type=float, help="number of generations to map to a single yeasr")
     parser.add_argument("--output", help="metadata with standardized dates and nonzero fitness records")
